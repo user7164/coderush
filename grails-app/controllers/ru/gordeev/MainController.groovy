4cb6a0281ae40e07e6eff9c27caff5e9565757a0
iroma <roma.gordeev@googlemail.com>
2014-03-20 17:10:27 +0400

    add main controller and reference views

---
+++ grails-app/controllers/ru/gordeev/MainController.groovy
@@ -0,0 +1,31 @@
+package ru.gordeev
+
+class MainController {
+
+    def index() {}
+
+    def about() {}
+
+    def blog() {}
+
+    def blogpost() {}
+
+    def contacts() {}
+
+    def portfolio2cols() {}
+
+    def portfolio3cols() {}
+
+    def portfolio4cols() {}
+
+    def scaffolding() {}
+
+    def shortcodes() {}
+
+    def singleportfolio() {}
+
+    def tables() {}
+
+    def typography() {}
+
+}
