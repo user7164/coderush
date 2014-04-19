9b1f5d465840cbd010b2aaf0a2963f6b486b9da4
rgordeev <rgordeev@naumen.ru>
2014-04-19 18:24:18 +0400

    changelog

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,60 +1,67 @@
 databaseChangeLog = {
 
-	changeSet(author: "user (generated)", id: "1397826831461-1") {
-		createTable(tableName: "tbl_author") {
-			column(autoIncrement: "true", name: "id", type: "bigint") {
-				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "tbl_authorPK")
+	changeSet(author: "user (generated)", id: "1397830700404-1") {
+		createTable(tableName: "TBL_AUTHOR") {
+			column(autoIncrement: "true", name: "ID", type: "BIGINT") {
+				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "CONSTRAINT_4")
 			}
 
-			column(name: "version", type: "bigint") {
+			column(name: "VERSION", type: "BIGINT") {
 				constraints(nullable: "false")
 			}
 
-			column(name: "creationDate", type: "timestamp") {
+			column(name: "CREATIONDATE", type: "TIMESTAMP") {
 				constraints(nullable: "false")
 			}
 
-			column(name: "name", type: "varchar(255)") {
-				constraints(nullable: "false")
-			}
-		}
-	}
-
-	changeSet(author: "user (generated)", id: "1397826831461-2") {
-		createTable(tableName: "tbl_post") {
-			column(autoIncrement: "true", name: "id", type: "bigint") {
-				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "tbl_postPK")
-			}
-
-			column(name: "version", type: "bigint") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "author_id", type: "bigint") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "comments", type: "varchar(255)") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "content", type: "text") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "creation_date", type: "timestamp") {
-				constraints(nullable: "false")
-			}
-
-			column(name: "tags", type: "varchar(255)")
-
-			column(name: "title", type: "varchar") {
+			column(name: "NAME", type: "VARCHAR(255)") {
 				constraints(nullable: "false")
 			}
 		}
 	}
 
-	changeSet(author: "user (generated)", id: "1397826831461-3") {
-		addForeignKeyConstraint(baseColumnNames: "author_id", baseTableName: "tbl_post", constraintName: "FKFE5B8811AE272B7", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "tbl_author", referencesUniqueColumn: "false")
+	changeSet(author: "user (generated)", id: "1397830700404-2") {
+		createTable(tableName: "TBL_POST") {
+			column(autoIncrement: "true", name: "ID", type: "BIGINT") {
+				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "CONSTRAINT_40")
+			}
+
+			column(name: "VERSION", type: "BIGINT") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "AUTHOR_ID", type: "BIGINT") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "COMMENTS", type: "VARCHAR(255)") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "CONTENT", type: "LONGVARCHAR") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "CREATION_DATE", type: "TIMESTAMP") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "TAGS", type: "VARCHAR(255)")
+
+			column(name: "TITLE", type: "VARCHAR(2147483647)") {
+				constraints(nullable: "false")
+			}
+		}
 	}
+
+	changeSet(author: "user (generated)", id: "1397830700404-3") {
+		addForeignKeyConstraint(baseColumnNames: "AUTHOR_ID", baseTableName: "TBL_POST", baseTableSchemaName: "PUBLIC", constraintName: "FKFE5B8811AE272B7", deferrable: "false", initiallyDeferred: "false", onDelete: "RESTRICT", onUpdate: "RESTRICT", referencedColumnNames: "ID", referencedTableName: "TBL_AUTHOR", referencedTableSchemaName: "PUBLIC", referencesUniqueColumn: "false")
+	}
+
+    changeSet(author: "Roman", id: "1397830700404-4") {
+        addColumn(tableName: "tbl_post")
+                {
+                    column(name: "tags1", type: "text")
+                }
+    }
 }
